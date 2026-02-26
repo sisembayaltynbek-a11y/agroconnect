6486e42bed7376b3ab87bf7f79799bc33f785cab
Altynbek <sisembayaltynbek@gmail.com>
2026-02-26 19:38:38 +0500

    changes1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asus\OneDrive\Документы\Django progs\farmer_project\venv
+set VIRTUAL_ENV=C:\Code\Projects\farmer_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
